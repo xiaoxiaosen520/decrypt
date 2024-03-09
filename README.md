be30bb8c5ba0039e165da72abfc6568905486b97
yss <xiaoxiaosen520@126.com>
2024-03-09 10:33:31 +0800

    modify npm

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # decrypt
+Using the cryptography library for AES encryption and descrypt
+
+encrypt_file(input_file_path, output_file_path)
+
+decrypted_data = decrypt_file(input_file_path)
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const { exec } = require('child_process');
 
-// 调用 Python 脚本
+// launch Python 
 exec('python decrypt.py', (error, stdout, stderr) => {
   if (error) {
-    console.error(`执行错误：${error.message}`);
+    console.error(`fail：${error.message}`);
     return;
   }
   if (stderr) {
-    console.error(`错误输出：${stderr}`);
+    console.error(`fial log：${stderr}`);
     return;
   }
-  console.log(`Python 脚本输出：${stdout}`);
+  console.log(`Python succees：${stdout}`);
 });
